e5482aa509b0d7a8089ae3f6f82bd36eb6090c9b
Subham Patel <140715377+Subham-Patel9547@users.noreply.github.com>
2024-12-23 13:30:34 +0530

    Update config.js

--- backend/db/config.js
+++ backend/db/config.js
@@ -26,7 +26,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/e-commerce", {
 
 
 
-// Simpe Code Node try and catch
+// Simpe Code No try and catch
 
 // const mongoose=require('mongoose');
 // mongoose.connect("mongodb://127.0.0.1:27017/e-commerce");
